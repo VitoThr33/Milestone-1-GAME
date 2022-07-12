4d0117e9aa266f9ec6178e3f5bb799a540903165
Ryan McGowan <vitomaserati@gmail.com>
2022-07-11 21:56:01 -0700

    to much research started lebron champinship dodge html/css and minimal java so far

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("keypress", handleStart,{once: true})
+const title= document.querySelector("[date-title]")
+//Game start functioin
+
+function gameStart( ){
+    title.classList.add("hide")
+
+
+
+}
+
+//Game Lose function 
+
+function gameLose(){
+
+
+
+}
